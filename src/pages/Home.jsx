b517e9b1bc4c4f47e034491372ae9099aebbc789
TBejarano24 <bejaranotomas04@gmail.com>
2025-06-11 18:02:44 -0300

    Se terminó el panel de información sobre horas de servicio, sólo hace falta añadir funcionalidad a los botones sobre tipos de servicio específico para mostrar más información

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -24,10 +24,10 @@ export default function Home() {
           image={"/homepage_service_bg2.webp"}
         />
       </div>
-      <h2 className="font-bold text-[24px] mt-[20px]">
+      <h2 className="font-bold text-[24px] mt-[20px] w-full max-w-[310px] text-center rounded-lg text-[#dadada] bg-[#0f47ad] sm:max-w-[600px]">
         Sobre las horas de servicio
       </h2>
-      <div className="w-[90%] max-w-[288px] bg-[#b4b4b4] mt-[20px] rounded-xl sm:max-w-[576px] grid grid-cols-1 justify-items-center">
+      <div className="w-[90%] max-w-[288px] bg-[#f2f3f7] mt-[20px] rounded-xl sm:max-w-[600px] grid grid-cols-1 justify-items-center">
         <div className="flex flex-col items-center max-w-[90%]">
           <img
             className="mt-[15px] rounded-xl"
@@ -35,10 +35,10 @@ export default function Home() {
             alt="joven indexando"
           />
           <p className="font-semibold text-[12px]">
-            "Cuando os halláis al servicio de vuestros semejantes, cólo estáis
+            "Cuando os halláis al servicio de vuestros semejantes, sólo estáis
             al servicio de vuestro Dios" (Mosíah 2:17)
           </p>
-          <h2 className="text-center text-[20px] font-bold my-[20px]">
+          <h2 className="text-center text-[20px] font-bold my-[20px] text-[#dadada] bg-[#0f47ad] w-full rounded-lg">
             ¿Por qué reportamos horas de servicio en FUNVAL?
           </h2>
           <p>
@@ -47,7 +47,11 @@ export default function Home() {
             manera simbólica paguen su beca prestando servicio significativo al
             prójimo.
           </p>
-          <img className="mt-[15px] rounded-xl" src="/info3.png" alt="" />
+          <img
+            className="mt-[15px] rounded-xl"
+            src="/info3.png"
+            alt="mujeres ayudando"
+          />
           <span className="my-[20px]">
             El presidente Spencer W. Kimball explicó:{" "}
             <span className="font-semibold">
@@ -56,21 +60,115 @@ export default function Home() {
               necesidades”
             </span>
           </span>
-          <h2 className="text-center text-[20px] font-bold my-[20px]">
+          <h2 className="text-center text-[20px] font-bold my-[20px] text-[#dadada] bg-[#0f47ad] w-full rounded-lg">
             Recomendaciones al reportar horas de servicio
           </h2>
-          <ol>
-            <li>
+          <img
+            className="rounded-xl mt-[20px]"
+            src="/info2.png"
+            alt="mujer joven en el templo"
+          />
+          <ol className="list-outside list-decimal max-w-[80%]">
+            <li className="my-[15px]">
               <p>
                 Las horas de servicio son la forma de pagar la beca que se te ha
-                brindando, sino reportar tus horas cada mes no podrás disfrutar
+                brindando, si no reportas tus horas cada mes no podrás disfrutar
                 de los beneficios de la beca.
               </p>
             </li>
-            <li></li>
-            <li></li>
-            <li></li>
+            <li className="my-[15px]">
+              <p>
+                Asegurate de subir fotos de{" "}
+                <span className="font-bold">antes</span>,{" "}
+                <span className="font-bold">durante</span> y{" "}
+                <span className="font-bold">después</span> del servicio en un
+                archivo <span className="font-bold">PDF</span> o{" "}
+                <span className="font-bold">Word</span> (Si no apareces en
+                ninguna de las fotos no será aceptado tu servicio). Esperamos
+                que las fotos que subas reflejen un impacto significativo en las
+                personas.
+              </p>
+            </li>
+            <li className="my-[15px]">
+              <p>
+                Asegúrate de no combinar los servicios que vas a reportar es
+                decir llena el formulario por cada tipo de servicio que hagas.
+              </p>
+            </li>
+            <li className="my-[15px]">
+              <p>
+                Las horas de servicio no son acumulativas, así que asegúrate de
+                solo reportar las horas requeridas para el nivel/modulo que
+                estas cursando.
+              </p>
+            </li>
           </ol>
+          <h2 className="font-bold text-[20px] text-center my-[20px] text-[#dadada] bg-[#0f47ad] w-full rounded-lg">
+            Principales actividades que cuentan como servicio
+          </h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2">
+            <button className="flex flex-col items-center hover:scale-105 ease-in-out duration-100 cursor-pointer">
+              <img
+                className="rounded-xl mt-[20px]"
+                src="/service1.png"
+                alt="Referir a un amigo"
+              />
+              <h3 className="font-semibold text-center">Referir a un amigo</h3>
+            </button>
+            <button className="flex flex-col items-center hover:scale-105 ease-in-out duration-100 cursor-pointer">
+              <img
+                className="rounded-xl mt-[20px]"
+                src="/service2.png"
+                alt="Actividades presenciales de servicio"
+              />
+              <h3 className="font-semibold text-center">
+                Actividades presenciales de servicio
+              </h3>
+            </button>
+            <button className="flex flex-col items-center hover:scale-105 ease-in-out duration-100 cursor-pointer">
+              <img
+                className="rounded-xl mt-[20px]"
+                src="/service3.png"
+                alt="Actividades de templo e historia familiar"
+              />
+              <h3 className="font-semibold text-center">
+                Actividades de templo e historia familiar
+              </h3>
+            </button>
+            <button className="flex flex-col items-center hover:scale-105 ease-in-out duration-100 cursor-pointer">
+              <img
+                className="rounded-xl mt-[20px]"
+                src="/service4.png"
+                alt="Otros servicios"
+              />
+              <h3 className="font-semibold text-center">
+                Otros servicios (considerados en Funval)
+              </h3>
+            </button>
+            <button className="flex flex-col items-center hover:scale-105 ease-in-out duration-100 cursor-pointer">
+              <img
+                className="rounded-xl mt-[20px]"
+                src="/service5.png"
+                alt="Actividad de la brújula"
+              />
+              <h3 className="font-semibold text-center">
+                Actividad de la brújula
+              </h3>
+            </button>
+            <button className="flex flex-col items-center hover:scale-105 ease-in-out duration-100 cursor-pointer">
+              <img
+                className="rounded-xl mt-[20px]"
+                src="/service6.png"
+                alt="Actividades grupales en Funval"
+              />
+              <h3 className="font-semibold text-center">
+                Actividades grupales en Funval
+              </h3>
+            </button>
+          </div>
+          <button className="text-[#dadada] text-[20px] bg-[#0f47ad] px-3 py-2 my-[25px] rounded-lg hover:cursor-pointer hover:scale-105 ease-in-out duration-100">
+            Volver al inicio
+          </button>
         </div>
       </div>
       <div className="w-full h-[180px] bg-[#13131380] mt-[18px]">
