a0dedcedc0467a4eb17d70570ae47c1b55e2c482
TBejarano24 <bejaranotomas04@gmail.com>
2025-06-16 14:04:13 -0300

    Se añadió react-router-dom a las dependencias, se crearon el header y el footer como layout y se añadieron varios svg para usar libremente

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
-import React from 'react'
-import AuthLayout from './components/layouts/AuthLayout'
-import Login from './pages/Login'
-import Forbidden from './pages/Forbidden'
-import Home from './pages/Home'
-import { Route, Routes } from 'react-router'
+import React from "react";
+import AuthLayout from "./components/layouts/AuthLayout";
+import HeaderFooter from "./Components/layouts/HeaderFooter";
+import Login from "./pages/Login";
+import Forbidden from "./pages/Forbidden";
+import Home from "./pages/Home";
+import { Route, Routes } from "react-router";
 
 export default function App() {
   return (
     <>
       <Routes>
         <Route element={<AuthLayout />}>
-          <Route path='/' element={<Home />} />
+          <Route path="/" element={<HeaderFooter />}>
+            <Route index element={<Home />} />
+          </Route>
         </Route>
-        <Route path='/login' element={<Login />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/forbiden" element={<Forbidden />} />
       </Routes>
     </>
-  )
+  );
 }
