53e4d6b286032c82e199a477b434229c018e3e41
NathanLoop2004 <Jinderloop@gmail.com>
2025-06-16 10:03:34 -0400

    Estilo de modal totalmente hecho

---
+++ src/Components/Modal_Users.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+
+export default function Modal_Users() {
+  
+  return (
+    <div className='bg-white w-[240px] h-[500px] ml-35 mt-[-5px] rounded-[10px] relative'>
+         <section className='flex flex-col'>
+          
+          {/* Botton de cerrar */}
+          <div className='w-[100%] h-[40px]  border-black flex justify-end'>
+            <button className=' border-black mr-5 mt-5 cursor-pointer'>
+          <img src='/icons8-cerrar.png' className='h-[30px] w-[30px] '/>
+          </button>
+          </div>
+          
+
+
+          {/* Logotipo Funval */}
+            <img src='/Funval-Photoroom.png' className='w-[210px] h-[80px] mt-2 ml-2 '/>
+
+
+            {/* Foto de perfil del usuario */}
+             <div className='  w-[160px] h-[130px] ml-8  flex  justify-center items-center'>
+               <img src='/Gatito.jpg' className=' w-[120px] h-[120px] rounded-full  object-cover'/>
+             </div>
+
+
+             {/* Nombre del usuario */}
+           <div className='flex flex-row  border-black relative'>
+             <h1 className='text-[22px] text-[#4e4e4ee0] font-semibold ml-5 mt-3  border-white '>NathanLoop2004</h1>
+            <img className='w-[20px] h-[25px] shadow-none border-white mt-4 right-4 absolute' src='/FunvalFlechita.webp'></img>
+           </div>
+
+
+           {/* gmail del usuario */}
+           <div className='flex flex-row  w-[100%] h-[20px] border-black relative'>
+            <h1 className='text-[16px] text-[#2c2c2ce0] ml-5 font-semibold border-white' >Jinderloop@gmail.com</h1>
+            <img className=' w-[20px] h-[20px] absolute right-4 mt-1' src='/logo_gmail.png'/>
+           </div>
+
+              {/* numero de telefono del usuario */}
+           <div className='flex flex-row  w-[100%] h-[20px] mt-2 border-black  relative'>
+              <h1 className='text-[13px] text-[#bd9100e0] ml-5 font-semibold border-white'>+595 981 048195</h1>
+              <img src='/whatsapp.png' className='w-[20px] h-[20px] absolute right-4'/>
+           </div>
+
+
+          {/* Button Changes Datos */}
+          <div className='w-[100%] border-black h-[40px] mt-15'>
+              <button className="flex flex-row w-[120px] h-[45px] cursor-pointer bg-[#317ae2]    hover:bg-[#013c8f] rounded-[11px] items-center p-3 gap-1  ml-15">
+                <h1 className='text-[8px] text-white font-bold ml-2'>CAMBIAR DATOS</h1>
+                <img src="/icons8-actualizar-30.png" alt="" className='w-[20px] h-[20px]'/>
+              </button>
+          </div>
+            
+
+
+
+
+
+
+         </section>
+    </div>
+  )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import Modal_Users from '../Components/Modal_Users'
 
 export default function Home() {
     return (
-        <div>
-            HOLA
+        <div className='bg-black w-[375px] h-[1000px]'>
+            
+              <Modal_Users/>
         </div>
     )
 }
+//  <button className='bg-white ml-65 mt-5 px-5 py-2 rounded-[26px]'>
+//                 Usuarios
+            
+//             </button>
