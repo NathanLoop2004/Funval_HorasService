c03efd9f11e9519cb168e554f291b255b1ee65b2
TBejarano24 <bejaranotomas04@gmail.com>
2025-06-17 12:38:06 -0300

    Se añadieron reglas en el css para personalizar la barra de scroll

--- src/Components/Home/HoursModal.jsx
+++ src/Components/Home/HoursModal.jsx
@@ -23,7 +23,7 @@ export default function HoursModal({ isOpenHours, closeModal }) {
         <p className="text-[24px] px-5 font-semibold text-[#fff] bg-[#0f47ad] text-center sm:text-[30px] sm:px-15 rounded-xl mb-5">
           Horas de servicio reportadas
         </p>
-        <div className="bg-[#ebebeb] h-[65%] w-[90%] max-w-[850px] grid grid-cols-1 justify-items-center overflow-scroll scroll-hidden pb-2">
+        <div className="bg-[#ebebeb] h-[65%] w-[90%] max-w-[850px] grid grid-cols-1 justify-items-center overflow-y-scroll custom-scroll pb-2">
           <ServiceCard />
           <ServiceCard />
           <ServiceCard />
